89259b8ed0df206cc2d7c870eabddf41360ed391
DevMehta22 <mehtadev907@gmail.com>
2024-10-10 22:39:23 +0530

    RSA and Digital_signature modified

--- Digital_signature.py
+++ Digital_signature.py
@@ -1,11 +1,24 @@
 import hashlib
+import math
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def find_e(phi):
+    e = 3
+    while gcd(e, phi) != 1:
+        e += 2  
+    return e
+
 
 def rsa_secrets():
     p = 61
     q = 53
     n = p * q
     phi = (p - 1) * (q - 1)
-    e = 17
+    e = find_e(phi)
     d = pow(e, -1, phi)
     
     public_key = [e,n]
@@ -24,13 +37,14 @@ def decrypt(text,public_key):
     n = public_key[1]
     decrypted_text = ''.join([chr((char**e)%n) for char in text])
     return decrypted_text
+
+private_key,public_key = rsa_secrets()
     
 plaintext = input("Enter the Plain text:")
 
 msg_digest = (hashlib.sha1(plaintext.encode())).hexdigest()
 print("Message_digest(sender):",msg_digest)
 
-private_key,public_key = rsa_secrets()
 
 signature_list = encrypt(msg_digest,private_key)
 print(signature_list)
--- RSA.py
+++ RSA.py
@@ -1,29 +1,43 @@
+import math
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def find_e(phi):
+    e = 3
+    while gcd(e, phi) != 1:
+        e += 2  
+    return e
+
 def rsa_secrets():
     p = 61
     q = 53
     n = p * q
     phi = (p - 1) * (q - 1)
-    e = 17
+    e = find_e(phi)
     d = pow(e, -1, phi)
     
-    public_key = [e,n]
-    private_key = [d,n]
+    public_key = [e, n]
+    private_key = [d, n]
     
-    return private_key,public_key
+    return private_key, public_key
 
-def encryption(plaintext,public_key):
-    encrypted_text = [(ord(char)**public_key[0])%public_key[1] for char in plaintext]
+def encryption(plaintext, public_key):
+    e = public_key[0]
+    n = public_key[1]
+    encrypted_text = [(ord(char)**e) % n for char in plaintext]
     return encrypted_text
 
-def decryption(ciphertext,private_key):
-    decrypted_text = ''.join([chr((char ** private_key[0]) % private_key[1]) for char in ciphertext])
+def decryption(ciphertext, private_key):
+    d = private_key[0]
+    n = private_key[1]
+    decrypted_text = ''.join([chr((char ** d) % n) for char in ciphertext])
     return decrypted_text
 
-plaintext = "HELLOWORLD"
-public_key,private_key = rsa_secrets()
-ciphertext = encryption(plaintext,public_key)
+public_key, private_key = rsa_secrets()
+plaintext = input("Enter the plaintext: ")
+ciphertext = encryption(plaintext, public_key)
 print("Encrypted Text:", ciphertext)
-print("Decrypted Text:", decryption(ciphertext,private_key))
-    
-
-    
+print("Decrypted Text:", decryption(ciphertext, private_key))
